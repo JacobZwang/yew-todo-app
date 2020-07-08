d720a39d9b18e7abe42686310a16d7448eb86270
JacobZwang <zwang.jacob@gmail.com>
2020-07-07 17:37:31 -0700

    main functions and html

---
+++ src/main.rs
@@ -0,0 +1,51 @@
+#[macro_use]
+extern crate yew;
+
+use yew::html::*;
+
+struct Model {
+    input: String,
+    todos: Vec<String>,
+}
+
+enum Msg {
+    Add,
+    Update(String),
+    Remove(usize),
+    RemoveAll,
+    Nothing,
+}
+
+fn update(_: &mut Context<Msg>, model: &mut Model, msg: Msg) {
+    match msg {
+        Msg::Add => {}
+        Msg::Update(s) => {}
+        Msg::Remove(i) => {}
+        Msg::RemoveAll => {}
+        Msg::Nothing => {}
+    }
+}
+
+fn view(model: &Model) -> Html<Msg> {
+    html! {
+        <div>
+            <h1>{"Todo App"}</h1>
+            <input />
+        </div>
+        <div>
+            <button>{"Delete all Todos!"}</button>
+        </div>
+        <div>
+            <ul>
+            </ul>
+        </div>
+    }
+}
+
+fn main() {
+    let model = Model {
+        todos: vec![],
+        input: "".to_string(),
+    };
+    program(model, update, view);
+}
